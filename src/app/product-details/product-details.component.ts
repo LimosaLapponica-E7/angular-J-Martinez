7be074431d2546bbdd31f4c952188508b5225cbe
LimosaLapponica-E7 <mart4695@morris.umn.edu>
2020-02-10 03:38:26 -0600

    Subscribe to route parameters, fetching the product according to productId

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -15,4 +15,10 @@ export class ProductDetailsComponent implements OnInit {
     private route: ActivatedRoute,
   ) { }
 
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+    this.product = products[+params.get('productId')];
+  });
+  }
+
 }
