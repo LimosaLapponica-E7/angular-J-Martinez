2dfa0b0e9fd09706fab82a7e7141b08b0b4b8544
LimosaLapponica-E7 <mart4695@morris.umn.edu>
2020-02-10 04:00:45 -0600

    Import cart service to product-details.component.ts

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { CartService } from '../cart.service';
 import { products } from '../products';
 
 @Component({
