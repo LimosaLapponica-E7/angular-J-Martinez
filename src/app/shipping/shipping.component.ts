20d3aa6d13b06e668f7c8a794e62a1bb101cf4fa
LimosaLapponica-E7 <mart4695@morris.umn.edu>
2020-02-10 04:45:52 -0600

    Inject cart service in ShippingComponent constructor

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -7,8 +7,10 @@ import { CartService } from '../cart.service';
 })
 export class ShippingComponent implements OnInit {
   shippingCosts;
-  constructor() { }
-
+  constructor(
+  private cartService: CartService
+) {
+}
   ngOnInit() {
   }
 
